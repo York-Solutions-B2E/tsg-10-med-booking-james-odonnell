b0071a12faf7ba67328e2c1485fb4daf4c6b4a6e
DrChokomo <chokomo+github@proton.me>
2024-11-29 12:54:59 -0500

    first commit

---
+++ spring-api/src/main/java/net/york/tsg/MedicalBookingApplication.java
@@ -0,0 +1,13 @@
+package net.york.tsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalBookingApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalBookingApplication.class, args);
+	}
+	
+}
