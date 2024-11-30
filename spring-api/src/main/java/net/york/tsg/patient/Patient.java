2d8d5bfcf59db04720567cb2336c9283dbdecdcb
DrChokomo <chokomo+github@proton.me>
2024-11-30 14:33:01 -0500

    feat: add definition for patients table

---
+++ spring-api/src/main/java/net/york/tsg/patient/Patient.java
@@ -0,0 +1,50 @@
+package net.york.tsg.patient;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "patient_id")
+    private Long id;
+
+	@NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String phoneNumber;
+
+    @NotNull
+    private LocalDateTime dob;
+
+}
