f3d9d2ac4ef14f76c2361489326702551597aa13
DrChokomo <chokomo+github@proton.me>
2024-12-03 08:52:38 -0500

    feat: add navbar and theme selector

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,16 +1,17 @@
 import {useState, createContext, useContext} from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Link} from 'react-router-dom';
 import {ThemeProvider, createTheme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Switch from '@mui/material/Switch';
+import Button from '@mui/material/Button';
 
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark'
-  }
-});
+const ThemeContext = createContext();
+export const useThemeContext = () => {
+  return useContext(ThemeContext);
+}
 
 const AppContext = createContext();
 export const useAppContext = () => {
@@ -22,11 +23,35 @@ const App = () => {
   const [admin, setAdmin] = useState(false);
   const [auth, setAuth] = useState(false);
 
+  const [theme, setTheme] = useState(
+    createTheme({
+      palette: {
+        mode: 'dark',
+      }
+    })
+  );
+
+  const switchTheme = () => {
+    let mode = 'light';
+    if (theme.palette.mode === 'light')
+      mode = 'dark';
+    setTheme(createTheme({
+      palette: {
+        mode: mode
+      }
+    }))
+  }
+
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <AppContext.Provider value={[admin, setAdmin, auth, setAuth]}>
+        <AppContext.Provider value={{admin: admin, setAdmin: setAdmin, auth: auth, setAuth: setAuth}}>
           <CssBaseline />
+          <ThemeContext.Provider value={[theme, switchTheme]}>
+            <NavBar />
+          </ThemeContext.Provider>
+
+          <Switch onChange={() => setAdmin(!admin)}/>
 
           <Routes>
             <Route exact path="/" element={<Home />} />
---
+++ react-ui/src/components/NavBar.js
@@ -0,0 +1,66 @@
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+import AppBar from '@mui/material/AppBar';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+
+import {useAppContext, useThemeContext} from '../App';
+
+const NavBar = () => {
+
+	const {admin, auth, setAuth} = useAppContext();
+	const [theme, switchTheme] = useThemeContext();
+
+	const [anchorEl, setAnchorEl] = useState(null);
+	const open = Boolean(anchorEl);
+
+	return (
+		<Box sx={{flexGrow: 1}}>
+      <AppBar position="static">
+        <Toolbar>
+        	
+          <Box sx={{display: 'flex', flexGrow: 4, justifyContent: 'center'}}>
+        		<Button component={Link} to="/appointments" color="inherit">Appointments</Button>
+        		<Button component={Link} to="/doctors" color="inherit">Doctors</Button>
+        		<Button component={Link} to="/patients" color="inherit">Patients</Button>
+          </Box>
+
+          <IconButton onClick={switchTheme} sx={{width: 48, height: 48}}>
+          	{theme.palette.mode === 'light' ?
+          		<DarkModeIcon /> :
+          		<LightModeIcon />
+          	}
+          </IconButton>
+
+        	<AccountCircle
+        		sx={{width: 40, height: 40}}
+        		onClick={(event) => setAnchorEl(event.currentTarget)} />
+
+        	<Menu
+        		id="basic-menu"
+        		anchorEl={anchorEl}
+        		onClose={() => setAnchorEl(null)}
+        		open={open}>
+        		<MenuItem onClick={() => setAnchorEl(null)}>My Appointments</MenuItem>
+        		<MenuItem onClick={() => {
+        			setAnchorEl(null);
+        			setAuth(!auth);
+        		}}>{auth ? <Typography>Logout</Typography> : <Typography>Login</Typography>}</MenuItem>
+        	</Menu>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+	);
+}
+
+export default NavBar;
