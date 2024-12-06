5e7d0a22a30b26f4f93862a959ecc8f19600b49c
DrChokomo <chokomo+github@proton.me>
2024-12-06 08:00:42 -0500

    feat: allow anonymous users to schedule appointments.

--- spring-api/src/main/java/net/york/tsg/security/SecurityConfiguration.java
+++ spring-api/src/main/java/net/york/tsg/security/SecurityConfiguration.java
@@ -33,17 +33,16 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-            // allow anonymous access to the root page
             .requestMatchers("/", "/index.html", "/static/**",
                 "/*.ico", "/*.json", "/*.png", "/api/auth",
-                "/doctors/specialization", "/appointments/patient",
-                "/specializations")
+                "/doctors/specialization", "/specializations",
+                "/appointments/patient", "/appointments/doctor")
                 .permitAll()
-
             // all other requests
             .anyRequest().authenticated());
-
+            
         http.csrf((csrf) -> csrf
+            .ignoringRequestMatchers("/patients/new", "/appointments/new") //allow add new patients without authentication
             .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()));
         http.addFilterAfter(new CookieCsrfFilter(), BasicAuthenticationFilter.class);
