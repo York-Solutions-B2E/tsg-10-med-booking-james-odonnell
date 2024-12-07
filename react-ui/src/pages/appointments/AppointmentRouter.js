e8df6f5b3df2d85fd4ba3991be59a2960ec391d3
DrChokomo <chokomo+github@proton.me>
2024-12-07 11:58:20 -0500

    feat: add appointment router and edit appointment page.

---
+++ react-ui/src/pages/appointments/AppointmentRouter.js
@@ -0,0 +1,17 @@
+import {Routes, Route} from 'react-router-dom';
+
+import MyAppointments from './MyAppointments';
+import EditAppointment from './EditAppointment';
+
+const AppointmentRouter = () => {
+
+	return (
+		<Routes>
+			<Route path="/list" element={<MyAppointments />} />
+			<Route path="/edit" element={<EditAppointment />} />
+		</Routes>
+	);
+
+}
+
+export default AppointmentRouter;
---
+++ react-ui/src/pages/appointments/EditAppointment.js
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+import Typography from '@mui/material/Typography';
+
+const EditAppointment = () => {
+
+	return (
+		<Typography>Edit Appointments</Typography>
+	);
+
+}
+
+export default EditAppointment;
