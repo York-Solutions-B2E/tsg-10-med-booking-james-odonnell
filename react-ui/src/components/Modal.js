ffa211b7bee0eba06a8828c665fe8248ce54100e
DrChokomo <chokomo+github@proton.me>
2024-12-07 17:09:34 -0500

    style: adjust text positioning to be less akward, and space out the buttons at the bottom.

--- react-ui/src/components/Modal.js
+++ react-ui/src/components/Modal.js
@@ -1,17 +1,21 @@
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 /**
  * @param {string} 																		title: The title text for the modal.
  * @param {object[{title: string, content: object}]}	content: The content to be shown on the modal.
  * @param {object[{title: string, action: function, disabled: boolean}]}	actions: The action the modal provides
 */
-const Modal = ({title, content, actions, open, setOpen}) => {
+const Modal = ({title, warning, content, actions, open, setOpen}) => {
+
+	if (content == null || actions == null)
+		return null;
 
 	const handleClose = () => {
 		setOpen(false);
@@ -20,18 +24,30 @@ const Modal = ({title, content, actions, open, setOpen}) => {
 	return (
 		<Dialog open={open} onClose={handleClose}>
 				<DialogTitle>{title}</DialogTitle>
+
+				<DialogContent>
 				{content.map((content, index) =>
-					<DialogContent key={index}>
+					<>
 						<Divider />
-						<DialogTitle>{content.title}</DialogTitle>
-						{content.content}
-					</DialogContent>
+						<Typography variant="h6" sx={{display: 'flex', justifyContent: 'center'}}>
+							{content.title}
+						</Typography>
+						<Box sx={{display: 'flex',justifyContent: 'center'}}>
+							{content.content}
+						</Box>
+					</>
 				)}
-			<DialogActions>
+				</DialogContent>
+				<Box sx={{display: 'flex',justifyContent: 'center'}}>
+					{warning}
+				</Box>
+
+			<DialogActions sx={{display: 'flex',justifyContent: 'space-between'}}>
 				{actions.map((action, index) =>
-					<Button onClick={action.action} key={index} disabled={action.disabled}>{action.title}</Button>
+					<Button key={index} onClick={action.action} disabled={action.disabled}>{action.title}</Button>
 				)}
 			</DialogActions>
+
 		</Dialog>
 	);
 
