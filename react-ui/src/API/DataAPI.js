fe45b270cdffb09ee7f224eb3cc04f5fd838b4d8
DrChokomo <chokomo+github@proton.me>
2024-12-06 19:12:41 -0500

    feat: add method for front-end API to make POST reqests to back-end.

--- react-ui/src/API/DataAPI.js
+++ react-ui/src/API/DataAPI.js
@@ -1,6 +1,8 @@
 class DataAPI {
 
-	static async get(endpoint, headers) {
+	static get = async(endpoint, headers) => {
+		if (endpoint === "" || endpoint == null)
+			return;
 		let data = null;
 		try {
 			const response = await fetch(endpoint, {
@@ -16,8 +18,28 @@ class DataAPI {
 			console.log(data);
 		} catch (error){
 			console.error(error);
+			return data;
 		}
 		return data;
+	}
+
+	static post = async (endpoint, headers, body) => {
+		if (endpoint === "" || endpoint == null)
+			return;
+		try {
+			const response = await fetch(endpoint, {
+				method: 'POST',
+				credentials: 'include',
+				headers: headers,
+				body: body
+			});
+			if (!response.ok)
+				throw new Error(`Response status: ${response.status}`);
+
+			return await response.text();
+		} catch (error) {
+			console.error(error);
+		}
 	}
 	
 }
