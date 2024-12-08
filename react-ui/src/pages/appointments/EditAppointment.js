d95df7d7e05d42bfab413ae2baef9f72e41e77a3
DrChokomo <chokomo+github@proton.me>
2024-12-07 22:56:06 -0500

    feat: add form to edit an appointment.

--- react-ui/src/pages/appointments/EditAppointment.js
+++ react-ui/src/pages/appointments/EditAppointment.js
@@ -1,11 +1,189 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
 
+import dayjs from 'dayjs';
+
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+import {useAppContext} from '../../App';
+import DataAPI from '../../API/DataAPI';
+import {useAppointmentContext} from './AppointmentContext';
+import EditForm from './components/EditForm';
 
 const EditAppointment = () => {
 
+	const {index} = useParams();
+	const {navigate, userEmail, setUserEmail} = useAppContext();
+	const {appointments, setAppointments} = useAppointmentContext();
+	const [specializations, setSpecializations] = useState([]);
+	const [appointment, setAppointment] = useState(appointments[index]);
+	const [doctors, setDoctors] = useState([]);
+	const [now] = useState(dayjs());
+	const [nineAM] = useState(now.set('hour', 9).startOf('hour'));
+	const [fourPM] = useState(now.set('hour', 16).startOf('hour'));
+	const [minDate, setMinDate] = useState(now);
+	const [minTime, setMinTime] = useState(null);
+	const [valid, setValid] = useState(true);
+	const [form, setForm] = useState({
+		specialization: {
+			id: appointment.doctor.specialization.id,
+			name: appointment.doctor.specialization.name,
+		},
+		doctor: {
+			id: appointment.doctor.id,
+			firstName: appointment.doctor.firstName,
+			lastName: appointment.doctor.lastName,
+		},
+		fullName: appointment.doctor.firstName + " " + appointment.doctor.lastName,
+		date: null,
+		time: null,
+		visitType: null,
+	});
+
+	useEffect(() => {
+		if (userEmail === null)
+			navigate("/list");
+		(async () => {
+			const data = await DataAPI.get("specializations");
+			if (data != null)
+				setSpecializations(data);
+			if (form.specialization.id != null) {
+				const data = await DataAPI.get("doctors/specialization", {
+					specializationId: form.specialization.id
+				});
+				setDoctors(data);
+			}
+		})();
+	}, [userEmail, form.specialization]);
+
+	useEffect(() => {
+		if (dayjs().isBefore(nineAM) || (form.date != null && dayjs().isBefore(form.date))) {
+			setMinTime(nineAM);
+		} else {
+			let time = now.add(1, 'hour').hour();
+			time = now.set('hour', time).startOf('hour');
+			if (now.isBetween(fourPM, nineAM.add(1, 'day'))) {
+				setMinTime(nineAM);
+				setMinDate(dayjs().add(1, 'day'));
+			} else {
+				setMinTime(time);
+			}
+		}
+	}, [form.date, fourPM, nineAM, now, form.date]);
+
+	useEffect(() => {
+		console.log(form);
+		console.log(
+			form.specialization.id !== null &&
+			form.doctor.id !== null &&
+			form.date !== null &&
+			form.time !== null &&
+			form.visitType !== null
+		);
+		setValid(
+			form.specialization.id !== null &&
+			form.doctor.id !== null &&
+			form.date !== null &&
+			form.time !== null &&
+			form.visitType !== null
+		);
+	}, [form]);
+
+	const handleChange = (value, type, e) => {
+		let field = null;
+		if (value === null)
+			setValid(false);
+		console.log(value, type, e);
+		if (type === "doctor" || type === "specialization") {
+			const {id} = e.target;
+			const fieldIndex = parseInt(id.charAt(id.length - 1));
+			if (isNaN(fieldIndex)) {
+				setValid(false);
+				setForm({
+					...form,
+					specialization:
+						(type === "specialization" ? value !== null ?
+							value : {
+								id: null,
+								name: ''
+							} : form["specialization"]
+						),
+					doctor: {
+						id: null,
+						firstName: '',
+						lastName: '',
+					},
+					fullName: ''
+				});
+				return;
+			}
+			type === "doctor" ?
+				field = {...doctors[fieldIndex]} :
+				field = {...specializations[fieldIndex]};
+		}
+		setForm({
+			...form,
+			doctor: (type === "doctor" && field !== null ?
+				{...field} : type === "specialization" ? {
+					id: null,
+					firstName: '',
+					lastName: ''
+				} : form.doctor
+			),
+			[type]: (type === "doctor" || type === "specialization" ?
+				{...field} : value),
+			fullName: (type === "doctor" ?
+				value : type === "specialization" ?
+				'' : form.fullName
+			)
+		});
+	}
+
+	const appointmentConflicts = (date) => {
+    let day = date.day();
+    if (day === 0 || day === 6)
+      return true;
+
+    if (appointments != null) {
+      for (let i = 0;i < appointments.length;i++) {
+      	if (i == index)
+      		continue;
+        let apptDate = dayjs(appointments[i].dateTime).get('date');
+        if (date.get('date') === apptDate && appointments[i].doctor.id === form.doctor.id)
+          return true;
+      }
+    }
+    return false;
+  }
+
 	return (
-		<Typography>Edit Appointments</Typography>
+		<Container sx={{mt: 8}}>
+			<EditForm props={{
+				appointment,
+				setAppointment,
+				specializations,
+				doctors,
+				minDate,
+				minTime,
+				form,
+				handleChange,
+				appointmentConflicts}}
+			/>
+			<Box sx={{display: 'flex', justifyContent: 'space-between', mt: 4}} >
+				<Button
+					onClick={() => navigate("/myappointments/list")}
+					variant="contained">
+					CANCEL</Button>
+				<Button
+					variant="contained"
+					disabled={!valid}>
+					SAVE
+				</Button>
+			</Box>
+		</Container>
 	);
 
 }
---
+++ react-ui/src/pages/appointments/components/EditForm.js
@@ -0,0 +1,87 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import Radio from '@mui/material/Radio';
+import dayjs from 'dayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {DateCalendar} from '@mui/x-date-pickers/DateCalendar';
+import {DigitalClock} from '@mui/x-date-pickers/DigitalClock';
+
+const EditForm = (props) => {
+
+	const {
+		appointment,
+		setAppointment,
+		specializations,
+		doctors,
+		minDate,
+		minTime,
+		form,
+		handleChange,
+		appointmentConflicts
+	} = props.props;
+
+	return (
+		<Paper sx={{pb: 4}} elevation={5} >
+			<Grid container spacing={4} >
+				<Grid item xs={12} sx={{display: 'flex', justifyContent: 'center'}} >
+					<Stack sx={{width: '50%'}}>
+						<Autocomplete
+							id="specialization"
+							value={form.specialization.name}
+							options={specializations.map((spec) => spec.name)}
+							onChange={(e, newValue) => handleChange(newValue, "specialization", e)}
+							renderInput={(params) => <TextField {...params} label="Specialization"/>}
+						/>
+						<Box sx={{m: 2}} />
+						<Autocomplete
+							id="doctor"
+							value={form.fullName}
+							disabled={form.specialization.name === ''}
+							options={doctors.map((doctor) => doctor.firstName + " " + doctor.lastName)}
+							onChange={(e, newValue) => handleChange(newValue, "doctor", e)}
+							renderInput={(params) => <TextField {...params} label="Doctor"/>}
+						/>
+					</Stack>
+				</Grid>
+				<Grid item xs={12} sx={{display: 'flex', justifyContent: 'center'}} >
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<Grid item md={4}>
+							<DateCalendar
+								required
+								disablePast
+								shouldDisableDate={appointmentConflicts}
+								minDate={minDate}
+								onChange={(value) => handleChange(value, "date")}
+							/>
+						</Grid>
+						<Grid item md={2} sx={{mx: 4}}>
+							<Stack>
+								<DigitalClock
+									disabled={form.date == null}
+									skipDisabled
+									minTime={minTime}
+									maxTime={dayjs().set('hour', 16).startOf('hour')}
+									onChange={(value) => handleChange(value, "time")}
+								/>
+								<RadioGroup sx={{width: '10%', mt: 4}}>
+									<FormControlLabel label="In person" control={<Radio onChange={(e) => handleChange(e.target.value, "visitType")}/>} value={"IN_PERSON"}/>
+									<FormControlLabel label="Telehealth" control={<Radio onChange={(e) => handleChange(e.target.value, "visitType")}/>} value={"TELEHEALTH"}/>
+								</RadioGroup>
+							</Stack>
+						</Grid>
+					</LocalizationProvider>
+				</Grid>
+			</Grid>
+		</Paper>
+	);
+
+}
+
+export default EditForm;
