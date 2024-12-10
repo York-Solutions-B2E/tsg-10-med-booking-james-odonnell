fdae2ba713b3481adf128ec357a15630cc2bfca4
DrChokomo <chokomo+github@proton.me>
2024-12-10 07:56:06 -0500

    add route to view a specific doctor

--- react-ui/src/pages/admin/AdminRouter.js
+++ react-ui/src/pages/admin/AdminRouter.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import AdminProvider from './AdminContext';
 import DoctorList from './DoctorList';
 import NewDoctor from './NewDoctor';
+import DoctorView from './DoctorView';
 import EditDoctor from './EditDoctor';
 
 const AdminRouter = () => {
@@ -12,6 +13,7 @@ const AdminRouter = () => {
 			<Routes>
 				<Route path="/doctors" element={<DoctorList />} />
 				<Route path="/doctors/new" element={<NewDoctor />} />
+				<Route path="/doctors/:idnex" element={<DoctorView />} />
 				<Route path="/doctors/:index/edit" element={<EditDoctor />} />
 			</Routes>
 		</AdminProvider>
