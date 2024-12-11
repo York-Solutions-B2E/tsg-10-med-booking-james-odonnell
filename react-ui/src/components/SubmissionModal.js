4fbee7b58a458f1683b8ddc5fb8a96c396f20834
DrChokomo <chokomo+github@proton.me>
2024-12-11 11:37:34 -0500

    remove unused props

--- react-ui/src/components/SubmissionModal.js
+++ react-ui/src/components/SubmissionModal.js
@@ -4,40 +4,12 @@ import Typography from '@mui/material/Typography';
 
 import Modal from '../components/Modal';
 
-const SubmissionModal = ({open, setOpen, cancel, confirmSubmit, form, patient, doctor, specialization, appointment}) => {
+const SubmissionModal = ({open, setOpen, cancel, confirmSubmit, form}) => {
 
 	const title = "Are you sure you want to submit?"
 
 	const warning = form.visitType !== null && form.visitType === "IN_PERSON" ?
 		<Typography color="lightBlue">Please arrive 15 minutes<br/>before your appointment.</Typography> : null
-
-	// const content = [ patient === null ? {} : {
-	// 		title: "Patient information",
-	// 		content:
-	// 			<p>
-	// 				First name: {patient.firstName}<br />
-	// 				Last name: {patient.lastName}<br />
-	// 				Date of birth: {dayjs(patient.dob).format("MM/DD/YYYY")}<br />
-	// 				Email: {patient.email}
-	// 			</p>
-				
-	// 	}, {
-	// 		title: "Doctor",
-	// 		content:
-	// 			<p>
-	// 				Doctor: {doctor.firstName} {doctor.lastName}<br />
-	// 				Specialization: {specialization.name}<br />
-	// 			</p>
-	// 	}, {
-	// 		title: "Details",
-	// 		content:
-	// 			<p>
-	// 				Date: {dayjs(form.date).format("MM/DD/YYYY")}<br />
-	// 				Time: {dayjs(form.time).format("hh:mm A")}<br />
-	// 				Visit type: {appointment.visitType}<br />
-	// 			</p>
-	// 	},
-	// ];
 
 	const content = [ form.patient == null ? {} : {
 			title: "Patient information",
